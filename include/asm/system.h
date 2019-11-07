e1ab05011b3818f289a21f4a2a04fd60b062bead
HongJunxin <875433256@qq.com>
2019-11-08 00:03:53 +0800

    add comment for include

--- include/asm/system.h
+++ include/asm/system.h
@@ -1,23 +1,23 @@
 #define move_to_user_mode() \
 __asm__ ("movl %%esp,%%eax\n\t" \
-	"pushl $0x17\n\t" \
-	"pushl %%eax\n\t" \
-	"pushfl\n\t" \
-	"pushl $0x0f\n\t" \
-	"pushl $1f\n\t" \
-	"iret\n" \
-	"1:\tmovl $0x17,%%eax\n\t" \
-	"mov %%ax,%%ds\n\t" \
+	"pushl $0x17\n\t" \		// 将堆栈段选择符(SS)入栈
+	"pushl %%eax\n\t" \		// 将堆栈指针值(esp)入栈
+	"pushfl\n\t" \			// 将标志寄存器(eflags)内容入栈
+	"pushl $0x0f\n\t" \     // 将 Task0 代码段选择符(cs)入栈
+	"pushl $1f\n\t" \		// 将下面标号 1 的偏移地址(eip)入栈
+	"iret\n" \				// 执行中断返回指令，则会跳转到下面的标号 1 处
+	"1:\tmovl $0x17,%%eax\n\t" \  // 此时开始执行任务 0
+	"mov %%ax,%%ds\n\t" \   // 初始化段寄存器指向本局部表的数据段
 	"mov %%ax,%%es\n\t" \
 	"mov %%ax,%%fs\n\t" \
 	"mov %%ax,%%gs" \
 	:::"ax")
 
-#define sti() __asm__ ("sti"::)
-#define cli() __asm__ ("cli"::)
-#define nop() __asm__ ("nop"::)
+#define sti() __asm__ ("sti"::)  // 开中断
+#define cli() __asm__ ("cli"::)  // 关中断
+#define nop() __asm__ ("nop"::)  // 空操作
 
-#define iret() __asm__ ("iret"::)
+#define iret() __asm__ ("iret"::)  // 中断返回
 
 // (0x8000+(dpl<<13)+(type<<8)) => desc_struct.b
 // addr(处理函数地址) => desc_struct.a
@@ -40,6 +40,8 @@ __asm__ ("movw %%dx,%%ax\n\t" \
 
 #define set_system_gate(n,addr) \
 	_set_gate(&idt[n],15,3,addr)
+
+// 设置段描述符函数
 
 #define _set_seg_desc(gate_addr,type,dpl,base,limit) {\
 	*(gate_addr) = ((base) & 0xff000000) | \
--- include/linux/head.h
+++ include/linux/head.h
@@ -6,15 +6,15 @@ typedef struct desc_struct {
 } desc_table[256];
 
 extern unsigned long pg_dir[1024];
-extern desc_table idt,gdt;
+extern desc_table idt,gdt;  // 中断描述符表，全局描述符表
 
-#define GDT_NUL 0
-#define GDT_CODE 1
-#define GDT_DATA 2
-#define GDT_TMP 3
+#define GDT_NUL 0		// 全局描述符表的第 0 项，不使用
+#define GDT_CODE 1		// 第 1 项 -- 内核的代码段描述符项
+#define GDT_DATA 2		// 第 2 项 -- 内核的数据段描述符项
+#define GDT_TMP 3		// 第 3 项 -- 系统描述符项，linux 没有使用
 
-#define LDT_NUL 0
-#define LDT_CODE 1
-#define LDT_DATA 2
+#define LDT_NUL 0		// 每个局部描述符表的第 0 项，不使用
+#define LDT_CODE 1		// 第 1 项 -- 用户程序的代码段描述符项
+#define LDT_DATA 2		// 第 2 项 -- 用户程序的数据段描述符项
 
 #endif
